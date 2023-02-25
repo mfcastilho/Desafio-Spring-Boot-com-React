80579c5c910e4ab8bc933d83f30820aa3ebe389d
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-02-25 20:23:12 -0300

    Implementing depositPage

--- frontend/src/RoutesApp.js
+++ frontend/src/RoutesApp.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes,Route} from "react-router-dom";
 import AuthAccountPage from "./authAccountPage/AuthAccountPage";
 import AuthPasswordPage from "./authPasswordPage/AuthPasswordPage";
 import UserPanel from "./userPanel/UserPanel";
+import DepositPage from "./depositPage/DepositPage";
+import TransferPage from "./transferPage/TransferPage";
 
 const RoutesApp = ()=>(
   <BrowserRouter>
@@ -9,6 +11,9 @@ const RoutesApp = ()=>(
       <Route path="/" element={<AuthAccountPage/>}/>
       <Route path="/password" element={<AuthPasswordPage/>}/>
       <Route path="/userPanel" element={<UserPanel/>}/>
+      <Route path="/userPanel/depositos" element={<DepositPage/>} />
+      <Route path="/userPanel/saques" element={<DepositPage/>} />
+      <Route path="/userPanel/transferencias" element={<TransferPage/>} />
     </Routes>
   </BrowserRouter>
 )
--- frontend/src/authAccountPage/AuthAccountPage.js
+++ frontend/src/authAccountPage/AuthAccountPage.js
@@ -1,14 +1,39 @@
 import React from "react";
 import "./AuthAccountPage.css";
+import axios from "axios";
+
+const url_base = "http://localhost:8080";
+
+
 
 const AuthAccountPage = ()=>{
+
+  // axios.get(`https://cors-anywhere.herokuapp.com/${url_base}/conta/67890`);
+
+  // axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+  // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+  // axios.get(`${url_base}/conta/67890`,{
+  //   method: "GET",
+  //   mode:"cors",
+  //   headers:{
+  //     "Content-Type": "apllication/json"
+  //   }
+  // })
+  // .then((response) =>console.log("resposta da requisição = "+response))
+  // .catch(error=> console.log("reqisição falhou"));
+
+  axios.get(`${url_base}/conta/67890`,)
+  .then((response) =>console.log("resposta da requisição = "+response))
+  .catch(error=> console.log("reqisição falhou"));
+  
 
  
   
 
   return(<div class="container">
   <section class="form-login">
-      <form class="w-75 mx-auto" action="/conta" method="GET">
+      <form >
           <h1 class="text-center mb-3 title">Banco Itaú</h1>
 
           <div class="mb-3">
